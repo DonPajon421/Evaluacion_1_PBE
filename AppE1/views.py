707b9d0dc597315caef9fc303489f10dc7d17963
Laboratorio 117 <CAL_LAB117@inacap.cl>
2026-09-14 12:37:09 -0300

    Creación vistas

--- AppE1/views.py
+++ AppE1/views.py
@@ -8,8 +8,4 @@ def mostrar_home(request):
     return render(request, "index.html")
 
 def mostrar_servicio(request):
-    servicio = {
-        "nombre": "Lavado de vehículos",
-        "valor": 10000
-    }
-    return render(request, "about.html", servicio)
+    return render(request, "about.html")
